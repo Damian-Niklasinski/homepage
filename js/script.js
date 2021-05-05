e279f30b985f8cfdbf2a939c04056fe905f63884
Damian Niklasiński <damianiklasinski@wp.pl>
2021-05-05 17:21:39 +0200

    Added new hideSchoolPicture and changeSize function

--- js/script.js
+++ js/script.js
@@ -1,37 +1,40 @@
 {
     const welcome = () => {
         console.log("Hej, bardzo mi miło że tu wpadłeś!");
-    }
+    };
 
     const toggleBackground = () => {
         const body = document.querySelector("body");
         body.classList.toggle("darkTheme");
     };
 
+    const changeSize = () => {
+        const myPicture = document.querySelector(".section__myPhoto");
+        const changeSizeButton = document.querySelector(".section__changeSizeButton");
+        if (changeSizeButton.innerText === "Powiększ rozmiar zdjęcia") {
+            (changeSizeButton.innerText = "Zmniejsz rozmiar zdjęcia");
+        }
+        else changeSizeButton.innerText = "Powiększ rozmiar zdjęcia";
+        myPicture.classList.toggle("mySmallPhoto");
+    };
+
+    const hideSchoolPicture = () => {
+        const hideSchoolPicture = document.querySelector(".section__hideSchoolPictureButton");
+        const schoolPicture = document.querySelector(".section__schoolPicture");
+        if (hideSchoolPicture.innerText === "Ukryj zdjęcie") {
+            hideSchoolPicture.innerText = "Pokaż zdjęcie";
+        } else hideSchoolPicture.innerText = "Ukryj zdjęcie";
+        schoolPicture.classList.toggle("hidePicture");
+    };
+
     const init = () => {
-        let changeSizeButton = document.querySelector(".section__changeSizeButton");
-        let myPicture = document.querySelector(".section__myPhoto");
-        let schoolPicture = document.querySelector(".section__schoolPicture");
-        let hideSchoolPicture = document.querySelector(".section__hideSchoolPictureButton");
-        let hidePicture = document.querySelector(".hidePicture");
         const changeTheme = document.querySelector(".section__changeThemeButton");
-
-        changeSizeButton.addEventListener("click", () => {
-            if (changeSizeButton.innerText === "Powiększ rozmiar zdjęcia") {
-                (changeSizeButton.innerText = "Zmniejsz rozmiar zdjęcia");
-            }
-            else changeSizeButton.innerText = "Powiększ rozmiar zdjęcia";
-            myPicture.classList.toggle("mySmallPhoto");
-        })
-
-        hideSchoolPicture.addEventListener("click", () => {
-            if (hideSchoolPicture.innerText === "Ukryj zdjęcie") {
-                hideSchoolPicture.innerText = "Pokaż zdjęcie";
-            } else hideSchoolPicture.innerText = "Ukryj zdjęcie";
-            schoolPicture.classList.toggle("hidePicture");
-        });
+        const changeSizeButton = document.querySelector(".section__changeSizeButton");
+        const hideSchoolPictureButton = document.querySelector(".section__hideSchoolPictureButton");
 
         changeTheme.addEventListener("click", toggleBackground);
+        changeSizeButton.addEventListener("click", changeSize);
+        hideSchoolPictureButton.addEventListener("click", hideSchoolPicture);
 
         welcome();
     };
